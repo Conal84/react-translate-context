305d9cdf75279bf01137d94de964f50e8cf78e1c
Conal Walsh <conal.walsh1@gmail.com>
2021-04-25 19:15:48 +0100

    Added components and context

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  };
+
   render() {
-    return <div className='ui container'>App</div>;
+    return (
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i
+            onClick={() => this.onLanguageChange('english')}
+            className='flag us'
+          ></i>
+          <i
+            onClick={() => this.onLanguageChange('dutch')}
+            className='flag nl'
+          ></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 
